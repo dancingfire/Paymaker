9e7879c42635802c8b90fb7a77dc00d24c443b85
Gord Funk <gord.funk@gmail.com>
2019-02-13 02:19:45 +1100

    Removing Quartz  - couldsn't get the scheduler working - too much hidden stuff happening with the context

---
+++ automation/email_notification.aspx.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Web.UI;
+
+public partial class email_notification : Page {
+    private bool blnMorningEmail = false;
+
+    protected void Page_init(object sender, EventArgs e) {
+        if (Valid.getBoolean("AUTORUN", false)) {
+            G.User.ID = -1;
+            G.User.UserID = -1;
+            G.User.RoleID = 1;
+        }
+
+        if (Valid.getBoolean("Check", false)) {
+            TimesheetCycle.testAutomatedEmails();
+        }
+
+    }
+
+    protected void Page_Load(object sender, EventArgs e) {
+        if (Valid.getBoolean("AUTORUN", false) && !IsPostBack) {
+           TimesheetCycle.checkAutomatedEmails();
+        }
+    }
+
+    protected void btnSendMorning_Click(object sender, EventArgs e) {
+        TimesheetCycle.checkAutomatedEmails();
+    }
+    
+}
