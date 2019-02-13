fcdcc512af3733e65e2ca338afaec7c7a8cb7612
Gord Funk <gord.funk@gmail.com>
2019-02-14 03:45:06 +1100

    Leave testing functionality

--- admin/leave_settings.aspx.cs
+++ admin/leave_settings.aspx.cs
@@ -7,6 +7,7 @@ public partial class leave_settings : Root {
     protected void Page_Load(object sender, System.EventArgs e) {
         if (!IsPostBack) {
             oConfigAdmin.addConfig("LEAVECATCHALLEMAIL", "jacqui.litvik@fletchers.net.au");
+            oConfigAdmin.addConfig("PERMITTEDLEAVETESTERS", "");
 
             if (!Page.IsPostBack) {
                 loadSettings();
@@ -17,9 +18,13 @@ public partial class leave_settings : Root {
     private void loadSettings() {
         oConfigAdmin.loadValuesFromDB();
         txtCatchallEmail.Text = oConfigAdmin.getValue("LEAVECATCHALLEMAIL");
+        G.UserInfo.loadList(ref lstPermittedUsers);
+        Utility.setListBoxItems(ref lstPermittedUsers, oConfigAdmin.getValue("PERMITTEDLEAVETESTERS"));
     }
 
     protected void btnUpdate_Click(object sender, EventArgs e) {
         oConfigAdmin.updateConfigValueSQL("LEAVECATCHALLEMAIL", Utility.formatForDB(txtCatchallEmail.Text));
+        oConfigAdmin.updateConfigValueSQL("PERMITTEDLEAVETESTERS", hdLeaveTesters.Value);
+
     }
 }
