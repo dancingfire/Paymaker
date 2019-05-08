5b863f22df509c6edadc15eb14e6a30d9ce05fd3
Gord Funk <gord.funk@gmail.com>
2019-05-07 21:18:58 -0500

    Add sale price to update page

--- main/sale_update_special.aspx.cs
+++ main/sale_update_special.aspx.cs
@@ -22,9 +22,14 @@ public partial class sale_update : Root {
         txtCode.Text = oS.Code + " - " + oS.Address;
         txtComments.Text = oS.Comments;
         txtSalesDate.Text = oS.SaleDate;
-        txtSettlementDate.Text = Utility.formatDate(oS.SettlementDate);
-        txtEntitlementDate.Text = Utility.formatDate(oS.EntitlementDate);
+        if (oS.SettlementDate != DateTime.MinValue) {
+            txtSettlementDate.Text = Utility.formatDate(oS.SettlementDate);
+        }
+        if (oS.EntitlementDate != DateTime.MinValue) {
+            txtEntitlementDate.Text = Utility.formatDate(oS.EntitlementDate);
+        }
         txtGrossCommission.Text = Utility.formatMoney(oS.GrossCommission);
+        txtSalePrice.Text = Utility.formatMoney(oS.SalePrice);
         txtConjCommission.Text = Utility.formatMoney(oS.ConjCommission);
         Utility.setListBoxItems(ref lstStatus, oS.Status.ToString());
         Utility.loadPayPeriodList(ref lstPayPeriod);
@@ -36,7 +41,10 @@ public partial class sale_update : Root {
 
     protected void btnUpdate_Click(object sender, EventArgs e) {
         sqlUpdate oSQL = new sqlUpdate("SALE", "ID", Convert.ToInt32(hdSaleID.Value));
+        Sale oS = new Sale(Convert.ToInt32(hdSaleID.Value), true, true);
+        string LogEntry = "";
         oSQL.add("STATUSID", lstStatus.SelectedValue);
+        
         if (txtSalesDate.Text == "")
             oSQL.addNull("SALEDATE");
         else
@@ -49,24 +57,41 @@ public partial class sale_update : Root {
             oSQL.addNull("SETTLEMENTDATE");
         else
             oSQL.add("SETTLEMENTDATE", txtSettlementDate.Text);
+
         oSQL.add("COMMENTS", txtComments.Text);
-        oSQL.add("GROSSCOMMISSION", txtGrossCommission.Text);
-        oSQL.add("CONJUNCTIONALCOMMISSION", txtConjCommission.Text);
+        if (oS.GrossCommission != Convert.ToDouble(txtGrossCommission.Text)) {
+            oSQL.add("GROSSCOMMISSION", txtGrossCommission.Text);
+            LogEntry += String.Format(" Gross commission changed from {0} to {1} <br/>", Utility.formatMoney(oS.SalePrice), Utility.formatMoney(Convert.ToDouble(txtSalePrice.Text)));
+        }
+        if (oS.ConjCommission != Convert.ToDouble(txtConjCommission.Text)) {
+            oSQL.add("CONJUNCTIONALCOMMISSION", txtConjCommission.Text);
+            LogEntry += String.Format(" Conj commission changed from {0} to {1} <br/>", Utility.formatMoney(oS.SalePrice), Utility.formatMoney(Convert.ToDouble(txtSalePrice.Text)));
+        }
+        if (txtSalePrice.Text == "")
+            txtSalePrice.Text = "0";
+
+        if (oS.SalePrice != Convert.ToDouble(txtSalePrice.Text)) {
+            oSQL.add("SALEPRICE", txtSalePrice.Text);
+            LogEntry += String.Format(" Sale price changed from {0} to {1} <br/>", Utility.formatMoney(oS.SalePrice), Utility.formatMoney(Convert.ToDouble(txtSalePrice.Text)));
+        }
 
         oSQL.add("LOCKCOMMISSION", lstLocked.SelectedValue);
         if (lstPayPeriod.SelectedValue == "-1")
             oSQL.addNull("PAYPERIODID");
         else
             oSQL.add("PAYPERIODID", lstPayPeriod.SelectedValue);
-        Sale oS = new Sale(Convert.ToInt32(hdSaleID.Value));
-        string szLogRecord = String.Format(@"Code: {0} modified<br/>
+      
+        string szLogRecord = String.Format(@"
+                    
+                    Code: {0} modified<br/>
                     Status: {2} changed to {3}<br/>
                     Pay Period: {4} changed to {5}</br>
                     Sale date: {6} changed to {7}</br>
-                    DB ID = {1}
+                    DB ID = {1} <br/>
+                    {8}
                     ", oS.Code, hdSaleID.Value, oS.Status, lstStatus.SelectedItem.Text,
                     oS.PayPeriodID, lstPayPeriod.SelectedItem.Text,
-                    oS.SaleDate, txtSalesDate.Text
+                    oS.SaleDate, txtSalesDate.Text, LogEntry
                     );
 
         DBLog.addGenericRecord(DBLogType.SaleData, szLogRecord, Convert.ToInt32(hdSaleID.Value));
